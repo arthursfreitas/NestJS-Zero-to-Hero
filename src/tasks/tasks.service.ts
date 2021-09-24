ed15c27852f542f6e2b7cad0ab5630931410db9b
Arthur Freitas <arthurssfreitas@gmail.com>
2021-09-23 22:46:25 -0300

    task modifications

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -23,4 +23,17 @@ export class TasksService {
     this.tasks.push(task);
     return task;
   }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
 }
